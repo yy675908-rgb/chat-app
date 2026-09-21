27cc4507afc5e12ece7e2b55e9f091e5a05cdaf3
yy675908-rgb <yy675908@gmail.com>
2026-09-21 08:07:04 +0800

    Prevent keyboard focus restoration after navigation

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class _KeyboardDismissNavigatorObserver extends NavigatorObserver {
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     _clearFocus();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _clearFocus());
     super.didPop(route, previousRoute);
   }
 
